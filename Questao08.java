ef6177b91be7639a29d95a531ecbeeb4c88030ae
Denis <denis.sousa@aluno.uece.br>
2022-10-17 11:06:43 -0300

    solution if question 08

---
+++ Questao08.java
@@ -0,0 +1,35 @@
+public abstract class Questao08 {
+    public String nome;
+    public float nota1;
+    public float nota2;
+
+    public Questao08(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public float getNota1() {
+        return nota1;
+    }
+
+    public void setNota1(float nota1) {
+        this.nota1 = nota1;
+    }
+
+    public float getNota2() {
+        return nota2;
+    }
+
+    public void setNota2(float nota2) {
+        this.nota2 = nota2;
+    }
+
+    abstract float calcularMedia();
+}
